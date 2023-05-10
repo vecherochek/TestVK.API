082eb8cd74647beebc0474efc25238db19e938ff
Polina Bureeva <pbureeva@gmail.com>
2023-05-10 15:07:21 +0300

    added middleware & bd connection

--- TestVK.API/Program.cs
+++ TestVK.API/Program.cs
@@ -1,22 +1,30 @@
-using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.EntityFrameworkCore;
 using TestVK.API.BLL.Services;
 using TestVK.API.DAL.Repositories;
 using TestVK.API.DAL.Repositories.Contexts;
+using TestVK.API.Middlewaries;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
 services.AddControllers();
 
-services.AddDbContext<UserInfoDbContext>();
+var connections = builder.Configuration.GetConnectionString("userDbConnection");
+services.AddDbContext<UserInfoDbContext>(o => o.UseNpgsql(connections));
+
+/*services.AddDbContext<UserInfoDbContext>();*/
 services.AddTransient<UserRepository>();
 services.AddTransient<UserStateRepository>();
 services.AddTransient<UserGroupRepository>();
 services.AddTransient<UserService>();
-
+/*services.AddScoped<IUserRepository, UserRepository>();
+services.AddScoped<IUserStateRepository, UserStateRepository>();
+services.AddScoped<IUserGroupRepository, UserGroupRepository>();
+services.AddScoped<IUserService, UserService>();*/
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
 
+/*
 services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
     .AddNegotiate();
 
@@ -25,6 +33,7 @@ services.AddAuthorization(options =>
     // By default, all incoming requests will be authorized according to the default policy.
     options.FallbackPolicy = options.DefaultPolicy;
 });
+*/
 
 var app = builder.Build();
 
@@ -35,9 +44,8 @@ if (app.Environment.IsDevelopment())
 }
 app.UseSwagger();
 app.UseSwaggerUI();
-
-//app.UseHttpsRedirection();
-
+app.UseHttpsRedirection();
+app.UseMiddleware<ErrorMiddleware>();
 //app.UseAuthorization();
 
 app.MapControllers();
