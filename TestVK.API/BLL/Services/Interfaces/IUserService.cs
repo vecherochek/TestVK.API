bfe93be2adec4b191b2151fb3cc2105cfa4eb7ba
Polina Bureeva <pbureeva@gmail.com>
2023-05-10 03:44:33 +0300

    created user service

---
+++ TestVK.API/BLL/Services/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using TestVK.API.BLL.Models;
+
+namespace TestVK.API.BLL.Services.Interfaces;
+
+public interface IUserService
+{
+    User GetUser(Guid id);
+    IEnumerable<User> GetAllUsers();
+    void DeleteUser(Guid id);
+    void CreateNewUser(string login, byte[] password, string userGroupCode);
+}
---
+++ TestVK.API/BLL/Services/UserService.cs
@@ -0,0 +1,78 @@
+﻿using TestVK.API.BLL.Models;
+using TestVK.API.BLL.Services.Interfaces;
+using TestVK.API.DAL.Repositories;
+
+namespace TestVK.API.BLL.Services;
+
+public class UserService: IUserService
+{
+    private readonly UserRepository _userRepository;
+    private readonly UserGroupRepository _userGroupRepository;
+    private readonly UserStateRepository _userStateRepository;
+
+    public UserService(UserRepository userRepository, UserGroupRepository userGroupRepository, UserStateRepository userStatepRepository)
+    {
+        _userRepository = userRepository;
+        _userGroupRepository = userGroupRepository;
+        _userStateRepository = userStatepRepository;
+    }
+
+    public User GetUser(Guid id)
+    {
+        var user = _userRepository.Get(id);
+        if (user is null)
+            throw new InvalidOperationException("User with this id does not exist");
+        
+        return user;
+    }
+
+    public IEnumerable<User> GetAllUsers()
+    {
+        return _userRepository.GetUsers();
+    }
+
+    public void DeleteUser(Guid id)
+    {
+        var user = GetUser(id);
+        var state = _userStateRepository.GetUserStateByCode("Blocked");
+        
+        if (state is not null)
+        {
+            user.UserStateId = state.Id;
+        }
+        
+        _userRepository.Update(user);
+        _userRepository.Save();
+    }
+
+    public void CreateNewUser(string login, byte[] password, string userGroupCode)
+    {
+        if (_userRepository.GetUserByLogin(login) is not null)
+            throw new InvalidOperationException("User with this login already exists");
+        
+        var state = _userStateRepository.GetUserStateByCode("Active");
+        var group = _userGroupRepository.GetUserGroupByCode(userGroupCode);
+        
+        if (group is null)
+            throw new InvalidOperationException("Such group does not exist");
+        if (state is null)
+            throw new InvalidOperationException("Such state does not exist");
+        
+        //TODO: если не сущетсвует активного админа то можно добавить нового 
+        if (userGroupCode == "Admin")
+            throw new InvalidOperationException("Admin already exists. More than one admin is not allowed");
+        
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Login = login,
+            Password = password,
+            CreatedDate = DateTime.Now,
+            UserGroupId = group.Id,
+            UserStateId = state.Id
+        };
+        
+        _userRepository.Create(user);
+        _userRepository.Save();
+    }
+}
