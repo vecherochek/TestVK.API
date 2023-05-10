956c7eb2703a4428e151fbbbd45eefa749cfa2e0
Polina Bureeva <pbureeva@gmail.com>
2023-05-10 03:45:47 +0300

    created user controller

---
+++ TestVK.API/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TestVK.API.BLL.Services;
+using TestVK.API.Requests;
+using TestVK.API.Responses;
+
+namespace TestVK.API.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class UserController: ControllerBase
+{
+    private readonly UserService _userService;
+
+    public UserController(UserService userService)
+    {
+        _userService = userService;
+    }
+    
+    [HttpGet("{userId}")]
+    public GetUserResponse Get(GetUserRequest request)
+    {
+        var user = _userService.GetUser(request.userId);
+        return new GetUserResponse(user);
+    }
+    
+    [HttpGet("all-players-list")]
+    public GetAllUsersResponse Get()
+    {
+        return new GetAllUsersResponse(
+            _userService.GetAllUsers());
+    }
+    
+    [HttpDelete("{userId}")]
+    public void DeleteUser(DeleteUserRequest request)
+    {
+        _userService.DeleteUser(request.userId);
+    }
+    
+    [HttpPut("add")]
+    public void AddUser(AddUserRequest request)
+    {
+        _userService.CreateNewUser(request.login, request.password, request.userGroupCode);
+    }
+}
