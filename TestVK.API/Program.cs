58ca44f8b90c4b5e4f8590175730d6b03970909a
Polina Bureeva <pbureeva@gmail.com>
2023-05-10 03:47:39 +0300

    added requirements

--- TestVK.API/Program.cs
+++ TestVK.API/Program.cs
@@ -1,18 +1,26 @@
 using Microsoft.AspNetCore.Authentication.Negotiate;
+using TestVK.API.BLL.Services;
+using TestVK.API.DAL.Repositories;
+using TestVK.API.DAL.Repositories.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 
-// Add services to the container.
+services.AddControllers();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddDbContext<UserInfoDbContext>();
+services.AddTransient<UserRepository>();
+services.AddTransient<UserStateRepository>();
+services.AddTransient<UserGroupRepository>();
+services.AddTransient<UserService>();
 
-builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
     .AddNegotiate();
 
-builder.Services.AddAuthorization(options =>
+services.AddAuthorization(options =>
 {
     // By default, all incoming requests will be authorized according to the default policy.
     options.FallbackPolicy = options.DefaultPolicy;
@@ -23,13 +31,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
