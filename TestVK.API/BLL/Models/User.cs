ac69cde84852f6c535932cdeaf2979972c00cf90
Polina Bureeva <pbureeva@gmail.com>
2023-05-10 03:42:01 +0300

    added models

---
+++ TestVK.API/BLL/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace TestVK.API.BLL.Models;
+
+public class User
+{ 
+    public Guid Id { get; set; }
+    public string Login { get; set; }
+    public byte[] Password { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public Guid UserGroupId { get; set; }
+    public Guid UserStateId { get; set; }
+}
---
+++ TestVK.API/BLL/Models/UserGroup.cs
@@ -0,0 +1,10 @@
+﻿namespace TestVK.API.BLL.Models;
+
+public class UserGroup
+{
+    public Guid Id { get; set; }
+    
+    public string Code { get; set; }
+    
+    public string Description { get; set; }
+}
