832af3192510824470b9980ac9471243baac82c7
Polina Bureeva <pbureeva@gmail.com>
2023-05-10 15:03:51 +0300

    connection to the database has been moved. relationships between tables have been added

--- TestVK.API/DAL/Repositories/Contexts/UserInfoDbContext.cs
+++ TestVK.API/DAL/Repositories/Contexts/UserInfoDbContext.cs
@@ -3,22 +3,22 @@ using TestVK.API.BLL.Models;
 
 namespace TestVK.API.DAL.Repositories.Contexts;
 
-public class UserInfoDbContext: DbContext
+public class UserInfoDbContext : DbContext
 {
-    public DbSet<User> Users{ get; set; }
+    public DbSet<User> Users { get; set; }
     public DbSet<UserGroup> UserGroups { get; set; }
     public DbSet<UserState> UserStates { get; set; }
-    
+
     public UserInfoDbContext(DbContextOptions<UserInfoDbContext> options)
         : base(options)
     {
         Database.EnsureCreated();
     }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql("Server=192.168.1.186;Port=5432;Database=postgres;Username=postgres;Password=qwerty");
-    }
+        optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=postgres;Username=postgres;Password=qwerty");
+    }*/
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -28,7 +28,18 @@ public class UserInfoDbContext: DbContext
             options.HasIndex(e => e.Login).IsUnique();
         });
 
-        builder.Entity<UserGroup>().HasKey(x => x.Id);
-        builder.Entity<UserState>().HasKey(x => x.Id);
+        builder.Entity<UserGroup>(options =>
+        {
+            options.HasKey(x => x.Id);
+            options.HasMany<User>()
+                .WithOne();
+        });
+
+        builder.Entity<UserState>(options =>
+        {
+            options.HasKey(x => x.Id);
+            options.HasMany<User>()
+                .WithOne();
+        });
     }
 }
