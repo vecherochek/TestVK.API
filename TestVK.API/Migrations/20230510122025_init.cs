8ff5770e4d85bbacd268809d47ea4a6040a59e97
Polina Bureeva <pbureeva@gmail.com>
2023-05-10 15:55:10 +0300

    created migrations

---
+++ TestVK.API/Migrations/20230510122025_init.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TestVK.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserGroups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Code = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGroups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserStates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Code = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserStates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Login = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<byte[]>(type: "bytea", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserGroupId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserStateId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_UserGroups_UserGroupId",
+                        column: x => x.UserGroupId,
+                        principalTable: "UserGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Users_UserStates_UserStateId",
+                        column: x => x.UserStateId,
+                        principalTable: "UserStates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserGroups",
+                columns: new[] { "Id", "Code", "Description" },
+                values: new object[,]
+                {
+                    { new Guid("a4408f31-cf9b-49d8-b19c-56387a58cdb1"), "Admin", "Admin" },
+                    { new Guid("f5b2ebd4-6446-4c45-9b91-cfacd5957818"), "User", "User" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserStates",
+                columns: new[] { "Id", "Code", "Description" },
+                values: new object[,]
+                {
+                    { new Guid("14058055-cb2f-449d-afb1-2f0525eaeaa4"), "Blocked", "User does not exist" },
+                    { new Guid("8347b2dc-8656-4380-80d6-9bf38c0644ae"), "Active", "User exists" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Login",
+                table: "Users",
+                column: "Login",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserGroupId",
+                table: "Users",
+                column: "UserGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserStateId",
+                table: "Users",
+                column: "UserStateId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "UserGroups");
+
+            migrationBuilder.DropTable(
+                name: "UserStates");
+        }
+    }
+}
